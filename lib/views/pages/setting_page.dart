c2596639e4053126181d010a38450f4bb1097bc3
Kudae Sithu <kudaesithu2@gmail.com>
2026-01-14 10:12:13 +0630

    refactor ProfilePage to StatelessWidget and simplify layout; add SettingPage to navigation

---
+++ lib/views/pages/setting_page.dart
@@ -0,0 +1,168 @@
+import 'package:flutter/material.dart';
+
+class SettingPage extends StatefulWidget {
+  const SettingPage({super.key});
+
+  @override
+  State<SettingPage> createState() => _SettingPageState();
+}
+
+class _SettingPageState extends State<SettingPage> {
+  TextEditingController nameController = TextEditingController();
+  TextEditingController emailController = TextEditingController();
+  
+  bool value = false;
+  double sliderValue = 0.0;
+  String? selectedValue;
+
+  @override
+  void dispose() {
+    nameController.dispose();
+    emailController.dispose();
+    super.dispose();
+  }
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Settings', style: TextStyle(fontSize: 16)),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Column(
+            children: [
+              DropdownButton(
+                value: selectedValue,
+                hint: Text('Select an item'),
+                items: [
+                  DropdownMenuItem(value: '1', child: Text('Item 1')),
+                  DropdownMenuItem(value: '2', child: Text('Item 2')),
+                  DropdownMenuItem(value: '3', child: Text('Item 3')),
+                ], 
+                onChanged: (value) {
+                  setState(() {
+                    selectedValue = value;
+                  });
+                },
+              ),
+      
+              SizedBox(height: 20),
+              
+              TextField(
+                controller: nameController,
+                onEditingComplete: () => setState(() {}),
+                decoration: InputDecoration(
+                  labelText: 'Your name',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              SizedBox(height: 20),
+              TextField(
+                controller: emailController,
+                onEditingComplete: () => setState(() {}),
+                decoration: InputDecoration(
+                  labelText: 'Your email',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              
+              Checkbox(value: value, onChanged:(value) {
+                setState(() {
+                  this.value = value!;
+                });
+              }),
+        
+              CheckboxListTile(
+                title: Text('Receive Newsletter'),
+                value: value, 
+                onChanged:(value) {
+                setState(() {
+                  this.value = value!;
+                });
+              },),
+        
+              Switch(value: value, onChanged:(newValue) {
+                setState(() {
+                  value = newValue;
+                });
+              }),
+              
+              SwitchListTile.adaptive(title: Text('Newsletter'), value: value, onChanged:(newValue) {
+                setState(() {
+                  value = newValue;
+                });
+              }),
+        
+              ValueListenableBuilder(
+                valueListenable: nameController, builder:(context, value, child) {
+                return Text('Your name is: ${nameController.text}');
+              }),
+        
+              ValueListenableBuilder(
+                valueListenable: emailController,
+                builder: (BuildContext context, dynamic value, Widget? child) {
+                  return Text('Your email is: ${emailController.text}');
+                },
+              ),
+        
+        
+              Slider(
+                value: sliderValue, 
+                max: 100,
+                divisions: 5,
+                onChanged: (newValue) {
+                  setState(() {
+                    sliderValue = newValue;
+                  });
+                // print(sliderValue);
+              }),
+        
+              GestureDetector(
+                child: 
+                  Image.asset('assets/images/bg.jpeg'), 
+                  onTap: () => (),
+              ),
+      
+              InkWell(
+                splashColor: Colors.cyan,
+                onTap: () => (),
+                child: Container(
+                  height: 70,
+                  width: double.infinity,
+                  color: Colors.white12,
+                ),
+              ),
+      
+              const SizedBox(height: 20),
+      
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  padding: EdgeInsets.symmetric(horizontal: 50, vertical: 20),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30)
+                  )
+                ),
+                onPressed: () => (),
+                child: Text('Button'),
+              ),
+      
+              OutlinedButton(
+                style: OutlinedButton.styleFrom(
+                  padding: EdgeInsets.symmetric(horizontal: 50, vertical: 20),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30)
+                  )
+                ),
+                onPressed: () => (),
+                child: Text('Button'),
+              )
+      
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/views/widget_tree.dart
+++ lib/views/widget_tree.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:my_project_1/data/notifiers.dart';
 import 'package:my_project_1/views/pages/home_page.dart';
 import 'package:my_project_1/views/pages/profile_page.dart';
+import 'package:my_project_1/views/pages/setting_page.dart';
 import 'package:my_project_1/widgets/navbar_widget.dart';
 
 List<Widget> pages = [
@@ -16,20 +17,25 @@ class WidgetTree extends StatelessWidget {
   Widget build(BuildContext context) {
     return ValueListenableBuilder(valueListenable: lightThemeNotifier, builder:(context, value, child) {
       return Scaffold(
-      appBar: AppBar(
-        title: Text('My Flutter', style: TextStyle(fontSize: 16)),
-        centerTitle: false,
-        actions: [
-          IconButton(onPressed: () => {lightThemeNotifier.value = !lightThemeNotifier.value}, icon: lightThemeNotifier.value ? Icon(Icons.dark_mode) : Icon(Icons.light_mode))
-        ],
-      ),
-      body: ValueListenableBuilder(
-        valueListenable:  currentIndexNotifier,
-        builder: (BuildContext context, dynamic value, Widget? child) {
-          return  pages.elementAt(value);
-        },
-      ),
-      bottomNavigationBar: NavbarWidget(),
+        appBar: AppBar(
+          title: Text('My Flutter', style: TextStyle(fontSize: 16)),
+          centerTitle: false,
+          actions: [
+            IconButton(onPressed: () => {lightThemeNotifier.value = !lightThemeNotifier.value}, icon: lightThemeNotifier.value ? Icon(Icons.dark_mode) : Icon(Icons.light_mode)),
+            IconButton(onPressed: () => {
+              Navigator.push(context, MaterialPageRoute(builder: (context){
+                return SettingPage();
+              })),
+            }, icon: Icon(Icons.settings))
+          ],
+        ),
+        body: ValueListenableBuilder(
+          valueListenable:  currentIndexNotifier,
+          builder: (BuildContext context, dynamic value, Widget? child) {
+            return  pages.elementAt(value);
+          },
+        ),
+        bottomNavigationBar: NavbarWidget(),
       );
     });
   }
